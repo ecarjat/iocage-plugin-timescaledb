4e7a61569dc511dbfbaceb5a0672521b36c306d2
Emmanuel Carjat <emmanuel.carjat@quanthouse.com>
2020-12-12 13:06:45 +0100

    added promscale

--- post_install.sh
+++ post_install.sh
@@ -4,10 +4,13 @@ sysrc postgresql_enable=YES
 /usr/local/etc/rc.d/postgresql initdb
 
 git clone https://github.com/timescale/promscale.git
-cd ./promscale
-cd cmd/promscale
-go install
+cd promscale/cmd/promscale
 go build ./...
 cp promscale /usr/local/bin
+
+git clone https://github.com/timescale/promscale_extension.git
+cd promscale_extension
+ln -s /usr/local/include/libintl.h /usr/local/include/postgresql/server/libintl.h
+gmake && gmake install
 
 sysrc promscale_enable=YES
